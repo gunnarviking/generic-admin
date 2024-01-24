11ccd11387fd5d68c3776577f6b3f71419de2ac3
Michael Thompson <micgtho@gmail.com>
2024-01-24 16:56:56 +0100

    testing endpoint

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def login_post(request):
+    if request.method == "POST":
+        print("hello")
+        data = json.loads(request.body.decode("utf-8"))
+        email = data.get("email")
+        password = data.get("password")
+        return JsonResponse({"status": "success", "message": "Login successful"})
