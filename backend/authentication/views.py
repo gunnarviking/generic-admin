205bc01347182db278b92e509b5618e9690adb65
Michael Thompson <micgtho@gmail.com>
2024-01-24 17:04:05 +0100

    endpoint communication

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from json import loads
+from django.http import JsonResponse
 
 
-# Create your views here.
+@csrf_exempt
 def login_post(request):
     if request.method == "POST":
         print("hello")
-        data = json.loads(request.body.decode("utf-8"))
+        data = loads(request.body.decode("utf-8"))
         email = data.get("email")
         password = data.get("password")
         return JsonResponse({"status": "success", "message": "Login successful"})
