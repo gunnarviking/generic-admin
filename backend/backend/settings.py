2bf98e75b77f74824a7ceb8130b552f08a10dedc
Michael Thompson <micgtho@gmail.com>
2024-01-24 13:36:10 +0100

    postgres database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "kaFR?n<-fXsJeE]U:4,7SG",
+        "HOST": "127.0.0.1",
+        "PORT": "8092",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
